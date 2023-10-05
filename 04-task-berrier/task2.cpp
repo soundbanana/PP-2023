19af488b1182ced8784236b7f9e4b47186be99e6
Daniil Chemaev <chemaev.ds@gmail.com>
2023-10-05 09:03:01 +0300

    Added 04 Task

---
+++ 04-task-berrier/task2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <omp.h>
+#include <iostream>
+int main(int argc, char *argv[])
+{
+  double x[10001];
+  double sum;
+  int i;
+  
+for(i=0; i<10000; i++)
+{ x[i]=1;
+}  
+#pragma omp parallel for shared(x),reduction(+:sum)
+for(i=0; i<10000; i++)
+sum += x[i];
+    std::cout << "Sum: " << sum << std::endl;
+//return 0;
+}
